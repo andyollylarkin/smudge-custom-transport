56ff558c3070cf6267c54adbf5a3e402cf22d692
Denis Davydov <follenlast2@gmail.com>
2023-11-22 18:11:32 +0300

    random port

--- transport/ws_transport/internal/conn_helper.go
+++ transport/ws_transport/internal/conn_helper.go
@@ -2,10 +2,17 @@ package internal
 
 import (
 	"fmt"
+	"math/rand"
 	"net"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+const (
+	minPortRange = 10_000
+	maxPortRange = 65_000
 )
 
 // NewWsConnAdapter create new websocket conn adapter. If req is nil -> return remote socket addr as is.
@@ -22,10 +29,7 @@ func createConnWithRequest(r *http.Request, wsconn *websocket.Conn) (*WsConnAdap
 	realIp := r.Header.Get("X-Real-IP")
 	// if X-Real-IP header is set, we behind nginx. Set connection remote address to X-Real-IP
 	if realIp != "" {
-		_, port, err := net.SplitHostPort(wsconn.RemoteAddr().String())
-		if err != nil {
-			return nil, fmt.Errorf("cant assign real remote addr: %w", err)
-		}
+		port := generateRandomPort()
 
 		addr, err := net.ResolveTCPAddr("tcp", net.JoinHostPort(realIp, port))
 		if err != nil {
@@ -42,4 +46,20 @@ func createConnWithRequest(r *http.Request, wsconn *websocket.Conn) (*WsConnAdap
 
 func createConnNoRequest(wsconn *websocket.Conn) (*WsConnAdapter, error) {
 	return NewWsConn(wsconn, wsconn.RemoteAddr()), nil
+}
+
+type IntRange struct {
+	min, max int
+}
+
+// get next random value within the interval including min and max
+func (ir *IntRange) NextRandom(r *rand.Rand) int {
+	return r.Intn(ir.max-ir.min+1) + ir.min
+}
+
+func generateRandomPort() string {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	ir := IntRange{minPortRange, maxPortRange}
+
+	return fmt.Sprintf("%d", ir.NextRandom(r))
 }
